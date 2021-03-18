c84edb24b38381fc13bb74fe699fd63c12af57d2
Sergey Fedorov <31002143+Punctuality@users.noreply.github.com>
2021-03-18 03:36:30 +0300

    Changed example

---
+++ lab1/src/main/scala/ru/ifmo/se/software/testing/lab1/Example.scala
@@ -0,0 +1,31 @@
+package ru.ifmo.se.software.testing.lab1
+
+import cats.effect.IO
+import ru.ifmo.se.software.testing.lab1.func.Func
+import ru.ifmo.se.software.testing.lab1.sorting.Sorting
+
+import scala.util.Random
+
+object Example extends App {
+  val atan = Func.atan[IO, Float]
+
+  println(atan(0.5.toFloat).unsafeRunSync())
+  println(
+    atan(1.5.toFloat)
+      .redeem(e => s"GOT ERROR: ${e.getLocalizedMessage}", r => s"GOT result: $r")
+      .unsafeRunSync()
+  )
+
+  val sorting: Sorting[IO, List, Int] = Sorting.bubble[IO, List, Int]
+  val list: List[Int] = Random.shuffle((0 until 500).toList)
+
+  println(list)
+  println(sorting.sort(list).unsafeRunSync())
+
+  println(sorting.lookup(list.toArray, 0).unsafeRunSync())
+  println(
+    sorting.lookup(list.toArray, 500)
+      .redeem(e => s"GOT ERROR: ${e.getLocalizedMessage}", r => s"GOT result: $r")
+      .unsafeRunSync()
+  )
+}

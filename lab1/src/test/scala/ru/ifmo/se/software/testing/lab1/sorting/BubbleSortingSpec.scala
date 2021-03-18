bb76625ca27ccd94b8e47944541cc827617fb553
Sergey Fedorov <31002143+Punctuality@users.noreply.github.com>
2021-03-18 14:30:03 +0300

    Added comparisons check for bubble sort

--- lab1/src/test/scala/ru/ifmo/se/software/testing/lab1/sorting/BubbleSortingSpec.scala
+++ lab1/src/test/scala/ru/ifmo/se/software/testing/lab1/sorting/BubbleSortingSpec.scala
@@ -19,11 +19,18 @@ class BubbleSortingSpec {
       Sorting.bubble
     }
 
-    def ioBubbleSortingWithMetrics[A: OrderingF[IO, *]: ClassTag]: (Sorting[IO, List, A], AtomicLong, AtomicLong) = {
+    case class Metrics(lookups: AtomicLong, swaps: AtomicLong, comparisons: AtomicLong)
+
+    def ioBubbleSortingWithMetrics[A: Ordering: ClassTag]: (Sorting[IO, List, A], Metrics) = {
       type ArrayOpsA[F[_]] = ArrayOps[F, A]
-      val metrics = new ArrayOpsMetrics[IO, A]
-      implicit val aos: ArrayOpsA[IO] = (metrics: ArrayOpsA[Mid[IO, *]]) attach ArrayOps.default[IO, A]
-      (Sorting.bubble, metrics.lookups, metrics.swaps)
+      type OrderingA[F[_]] = OrderingF[F, A]
+      val arrMetrics = new ArrayOpsMetrics[IO, A]
+      val ordMetrics = new OrderingMetrics[IO, A]
+      implicit val aos: ArrayOpsA[IO] =
+        (arrMetrics: ArrayOpsA[Mid[IO, *]]) attach ArrayOps.default[IO, A]
+      implicit val ord: OrderingA[IO] =
+        (ordMetrics: OrderingA[Mid[IO, *]]) attach OrderingF.orderingLift[IO, A]
+      (Sorting.bubble, Metrics(arrMetrics.lookups, arrMetrics.swaps, ordMetrics.comparisons))
     }
 
     def naturals(n: Int): List[Int] = Iterator.range(0, n).toList
@@ -33,9 +40,16 @@ class BubbleSortingSpec {
       val swaps   = new AtomicLong(0)
 
       override def lookup(collection: Array[A], idx: Int): Mid[F, A] =
-        (fa: F[A]) => Applicative[F].pure(lookups.getAndIncrement) *> fa
+        (fa: F[A]) => Applicative[F].pure(lookups.incrementAndGet) *> fa
       override def swap(collection: Array[A], idxA: Int, idxB: Int): Mid[F, Unit] =
         (fa: F[Unit]) => Applicative[F].pure(swaps.incrementAndGet) *> fa
+    }
+
+    final class OrderingMetrics[F[_]: Monad, A] extends OrderingF[Mid[F, *], A] {
+      val comparisons = new AtomicLong(0)
+
+      override def compareF(a: A, b: A): Mid[F, Int] =
+        (fa: F[Int]) => Applicative[F].pure(comparisons.incrementAndGet) *> fa
     }
   }
 
@@ -76,22 +90,24 @@ class BubbleSortingSpec {
     val data:     List[Int] = naturals(100)
     val reversed: List[Int] = data.reverse
 
-    val (sorting, lookups, swaps) = ioBubbleSortingWithMetrics[Int]
+    val (sorting, Metrics(lookups, swaps, comparisons)) = ioBubbleSortingWithMetrics[Int]
 
     assertEquals(data, sorting.sort(reversed).unsafeRunSync())
     assertEquals(9900, lookups.get())
     assertEquals(4950, swaps.get())
+    assertEquals(4950, comparisons.get())
   }
 
   @Test
   def testSortingSortedList(): Unit = new TestEnv {
     val data:     List[Int] = naturals(100)
 
-    val (sorting, lookups, swaps) = ioBubbleSortingWithMetrics[Int]
+    val (sorting, Metrics(lookups, swaps, comparisons)) = ioBubbleSortingWithMetrics[Int]
 
     assertEquals(data, sorting.sort(data).unsafeRunSync())
     assertEquals(198, lookups.get())
     assertEquals(0, swaps.get())
+    assertEquals(99, comparisons.get())
   }
 
   @Test
@@ -99,11 +115,12 @@ class BubbleSortingSpec {
     val data:       List[Int] = naturals(100)
     val tailHeaded: List[Int] = data.last :: data.init
 
-    val (sorting, lookups, swaps) = ioBubbleSortingWithMetrics[Int]
+    val (sorting, Metrics(lookups, swaps, comparisons)) = ioBubbleSortingWithMetrics[Int]
 
     assertEquals(data, sorting.sort(tailHeaded).unsafeRunSync())
     assertEquals(198 + 196, lookups.get())
     assertEquals(99, swaps.get())
+    assertEquals(197, comparisons.get())
   }
 }
 
